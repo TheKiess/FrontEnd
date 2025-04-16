5306ded036d282672473915162bc20d4e1da416c
Frank Kiess <frank_kiess.junior@hotmail.com>
2025-04-15 23:13:00 -0300

    Amanhã fazer o css

---
+++ Backend/login.js
@@ -0,0 +1,15 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+  
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const errorMessage = document.getElementById("errorMessage");
+  
+    if (email === "admin@email.com" && password === "123456") {
+      errorMessage.textContent = "";
+      alert("Login realizado com sucesso!");
+
+    } else {
+      errorMessage.textContent = "E-mail ou senha inválidos!";
+    }
+  });
