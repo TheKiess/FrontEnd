9a1499b4ced223a4885b4bc646114d60e7f44a9e
Frank Kiess <frank_kiess.junior@hotmail.com>
2025-04-18 21:51:00 -0300

    Iniciando um DashBoard.

--- Backend/login.js
+++ Backend/login.js
@@ -1,15 +1,15 @@
 document.getElementById("loginForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-  
-    const email = document.getElementById("email").value.trim();
-    const password = document.getElementById("password").value.trim();
-    const errorMessage = document.getElementById("errorMessage");
-  
-    if (email === "admin@email.com" && password === "123456") {
-      errorMessage.textContent = "";
-      alert("Login realizado com sucesso!");
+  e.preventDefault();
 
-    } else {
-      errorMessage.textContent = "E-mail ou senha inválidos!";
-    }
-  });
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const errorMessage = document.getElementById("errorMessage");
+
+  if (email === "admin@email.com" && password === "123456") {
+    errorMessage.textContent = "";
+    alert("Login realizado com sucesso!");
+    window.location.href = "dashboard.html";
+  } else {
+    errorMessage.textContent = "E-mail ou senha inválidos!";
+  }
+});
